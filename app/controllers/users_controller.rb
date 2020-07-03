280dd12e9d921eaa6f1d4753e9a98d263788c563
administrator <admin@administrators-MacBook-Pro.local>
2020-07-03 13:11:25 +0900

    User controller内アクションの分割

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,10 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    # ここで、paginateメソッドを使用しているためviewsでは引数を指定する必要はない
     @users = User.paginate(page: params[:page])
-
-    @search_params = user_search_params
-
+    # 絞り込み検索
+    @prefecture_names = JpPrefecture::Prefecture.all
   end
-
-  # def search
-  #   lat = params[:latitude]
-  #   lng = params[:longitude]
-  #   range = params[:range]
-
-  #   @users = User.all.within(range, origin: [lat, lng])
-  #   # 現在地から最も近いユーザーのidを取得
-  #   @closest_user_id = @users.closest(origin: [lat, lng]).id
-  # end
 
   def show
     @user = User.find(params[:id])
@@ -43,29 +31,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # def available_baggage_new
-  #   @available_baggage = User.new()
-  # end
-
-  # def available_baggage
-  #   @available_baggage = User.new(available_baggage_params)
-  #   if @available_baggage.save
-  #     flash[:success] = "編集に成功しました"
-  #     redirect_to root_url
-  #   else
-  #     render "available_baggage_new"
-  #   end
-  # end
-
   def edit
-    # debugger
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # debugger
       @user.update_attributes(state: @user.prefecture_name)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -78,25 +50,6 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User delete"
     redirect_to users_url
-  end
-
-  def leaved_baggage
-    @transaction = Transaction.new
-  end
-
-  def leaved
-    @transaction = Transaction.new(request_params)
-    if @transaction.save
-      flash[:success] = "送信に成功しました"
-      # redirect先の設定
-    else
-      render 'leaved_baggage'
-    end
-  end
-
-  # 受信したリクエストを表示させるアクション
-  def received_baggage
-
   end
 
   def following
@@ -135,28 +88,4 @@ class UsersController < ApplicationController
                                   :about_baggage_size_h])
     end
 
-    def request_params
-      params.require(:request).permit(:request_content,
-                                      :baggage_content,
-                                      :from_day,
-                                      :from_time,
-                                      :to_day,
-                                      :to_time,
-                                      :transaction_message)
-    end
-
-    def user_search_params
-      # permit内にデータが内場合に例外を発生させないための記法
-      params.fetch(:search, {}).permit(:name, :sex, :state, 
-                                      :city, :street, :house)
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
 end
