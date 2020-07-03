1fba7ce79983e357ec676961371a49bb259b9c10
administrator <admin@administrators-MacBook-Pro.local>
2020-07-03 13:13:46 +0900

    model名変更(メソッド名がrequestとなったことによる変数の衝突エラー発生)

--- app/models/request.rb
+++
@@ -1,6 +0,0 @@
-class Request < ApplicationRecord
-    belongs_to :leaver, class_name: "User"
-    belongs_to :receiver, class_name: "User"
-    belongs_to :transactions, class_name: "Transaction"
-end
-　
