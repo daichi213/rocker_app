4a8d4c9bb551588a1852043acbabd06f9bd2322c
administrator <admin@administrators-MacBook-Pro.local>
2020-08-17 16:30:57 +0900

    メッセージ機能実装中

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,8 +2,10 @@ class Api::MessagesController < ApplicationController
   def index
     @baggage_request_to_user = BaggageRequestToUser.find_by(id: params[:baggage_request_to_id])
     @messages = @baggage_request_to_user.messages
-    last_message_id = params[:id].to_i
+    # params[:id]にAjaxで指定した値が入っていない
+    # →User.idの値が格納されてしまっている
     debugger
+    last_message_id = params[:id].to_i
     @messages = @baggage_request_to_user.messages.includes(:user).where("id > ?", last_message_id)
   end
 end
