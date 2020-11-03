233a1c48758c95232504cdfc2cfa5178f60b4750
administrator <admin@administrators-MacBook-Pro.local>
2020-11-03 23:54:20 +0900

    実装中

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,6 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
 port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
@@ -16,7 +15,7 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # socket
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 daemonize true
 stdout_redirect "#{Rails.root}/log/stdout", "#{Rails.root}/log/stderr"
 # Specifies the number of `workers` to boot in clustered mode.
